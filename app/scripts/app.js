c65e52b0d3be2cd95409633514916c6562901304
Jakub Mrozek <jakub.mrozek@gmail.com>
2013-09-16 00:45:19 +0200

    Init commit with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('linganoWebexpoApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
